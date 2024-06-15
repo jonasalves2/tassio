b61c2c18bfece7d0fb2f5a00324b86802c087807
Jonas Alves <134170627+jonasalves2@users.noreply.github.com>
2024-06-14 21:30:38 -0300

    scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+    document.querySelector('header').classList.toggle('dark-mode');
+    document.querySelector('main').classList.toggle('dark-mode');
+    document.querySelector('footer').classList.toggle('dark-mode');
+    document.querySelector('h2').classList.toggle('dark-mode');
+}
